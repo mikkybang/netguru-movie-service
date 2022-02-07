df25b515563dca38306bd106aa74a15e45b983b9
Michael Efemena <mefemena@gmail.com>
2022-02-07 03:25:39 +0100

    creat6e movie routes

---
+++ controller/movies.controller.js
@@ -0,0 +1,41 @@
+const { create, get } = require("../services/movies");
+
+const createMovies = (req, res) => {
+  try {
+    if (!req.body.title) throw new Error("title must be provided");
+    const data = {
+      title: req.body.title,
+    };
+    const result = await create(data, req.user);
+
+    res.status(201).json({
+      message: "Movie created successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Error creating movie",
+      error: error.message,
+    });
+  }
+};
+
+const getUserMovie = async (req, res) => {
+  try {
+    const result = await get(req.user);
+    res.status(201).json({
+      message: "Movies",
+      data: result,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Error finding movies",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = {
+  createMovies,
+  getUserMovie,
+};
---
+++ routes/movies.route.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const authenticate = require("../middleware/auth");
+const {
+  createMovies,
+  getUserMovie,
+} = require("../controller/movies.controller");
+
+router
+  .route("/")
+  .post(authenticate, createMovies)
+  .get(authenticate, getUserMovie);
